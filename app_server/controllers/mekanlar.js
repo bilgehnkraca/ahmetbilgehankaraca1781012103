d252b89e053dd790380201407ff5f7fa4ebb13c3
bilgehnkraca <bilgekrla04@gmail.com>
2020-11-29 14:33:22 +0300

    ikinci gönderim

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -7,6 +7,22 @@ const mekanBilgisi=function(req, res, next) {
   res.render('mekan-detay', { title: 'Mekan Bilgisi'});
 }
 
+const abdulcanbaz=function(req, res, next) {
+  res.render('mekan-abdul', { title: 'Mekan Abdulcanbaz'});
+}
+
+const burcfırın=function(req, res, next) {
+  res.render('mekan-burc', { title: 'Mekan Burc'});
+}
+
+const alacatı=function(req, res, next) {
+  res.render('mekan-alacatı', { title: 'Mekan Alacatı'});
+}
+
+const gramafonbox=function(req, res, next) {
+  res.render('mekan-gramafonbox', { title: 'Mekan Gramafon Box'});
+}
+
 const yorumEkle=function(req, res, next) {
   res.render('yorum-ekle', { title: 'Yorum Ekle' });
 }
@@ -14,5 +30,9 @@ const yorumEkle=function(req, res, next) {
 module.exports={
 anaSayfa,
 mekanBilgisi,
+abdulcanbaz,
+burcfırın,
+alacatı,
+gramafonbox,
 yorumEkle
 }
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,10 +5,10 @@ var ctrlDigerleri=require('../controllers/digerleri');
 
 router.get('/', ctrlMekanlar.anaSayfa);
 router.get('/Starbucks', ctrlMekanlar.mekanBilgisi);
-router.get('/abdulcanbaz_book_&_vintage_cafe', ctrlMekanlar.mekanBilgisi);
-router.get('/burc_firin', ctrlMekanlar.mekanBilgisi);
-router.get('/alacati_muhallebicisi', ctrlMekanlar.mekanBilgisi);
-router.get('/gramafon_box_cafe', ctrlMekanlar.mekanBilgisi);
+router.get('/abdulcanbaz_book_&_vintage_cafe', ctrlMekanlar.abdulcanbaz);
+router.get('/burc_firin', ctrlMekanlar.burcfırın);
+router.get('/alacati_muhallebicisi', ctrlMekanlar.alacatı);
+router.get('/gramafon_box_cafe', ctrlMekanlar.gramafonbox);
 router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle);
 router.get('/hakkinda', ctrlDigerleri.hakkinda);
 module.exports = router;
